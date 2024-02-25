b25937340ce85c0bac0fd2b06cf9da8edfdd8f3c
Fadel ellah ERRAMI <fadelellaherrami@gmail.com>
2024-02-25 23:54:02 +0100

    add concerns

--- app/models/concerns/moderable.rb
+++ app/models/concerns/moderable.rb
@@ -1,4 +1,38 @@
+require 'net/http'
+
 module Moderable
   extend ActiveSupport::Concern
 
+  included do
+    before_save :moderate_content
+  end
+
+  def moderate_content
+    # Accéder aux valeurs des colonnes spécifiées
+    text_to_moderate = self.text
+    language_using_to_moderate = self.language
+
+    # Code pour appeler l'API de modération et stocker le résultat dans is_accepted
+    url = URI("https://moderation.logora.fr/predict?text=#{URI.encode_www_form_component(text_to_moderate)}&language=#{URI.encode_www_form_component(language_using_to_moderate)}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["Content-Type"] = 'multipart/form-data; boundary=---011000010111000001101001'
+    request["User-Agent"] = 'insomnia/8.6.1'
+
+    response = http.request(request)
+    prediction = (JSON.parse(response.read_body)["prediction"]["0"])
+
+    self.is_accepted = prediction < 0.98
+
+    puts "============================================Prediction: #{prediction}"
+    puts "============================================Is accepted: #{self.is_accepted}"
+  end
+end
+
+class ModeratedModel < ApplicationRecord
+  include Moderable
 end
--- app/models/moderated_model.rb
+++ app/models/moderated_model.rb
@@ -1,36 +1,6 @@
 require 'net/http'
+require_relative './concerns/moderable'
 
 class ModeratedModel < ApplicationRecord
-  before_save :moderate_content
-
-  def moderate_content
-    # Accéder aux valeurs des colonnes spécifiées
-    text_to_moderate = self.text
-    language_using_to_moderate = self.language
-
-    # Code pour appeler l'API de modération et stocker le résultat dans is_accepted
-    url = URI("https://moderation.logora.fr/predict?text=#{URI.encode_www_form_component(text_to_moderate)}&language=#{URI.encode_www_form_component(language_using_to_moderate)}")
-
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Get.new(url)
-    request["Content-Type"] = 'multipart/form-data; boundary=---011000010111000001101001'
-    request["User-Agent"] = 'insomnia/8.6.1'
-
-    response = http.request(request)
-    prediction = (JSON.parse(response.read_body)["prediction"]["0"])
-
-    if prediction < 0.98
-      self.is_accepted = true
-    else
-      self.is_accepted = false
-    end
-
-    puts "============================================Prediction: #{prediction}"
-    puts "============================================Is accepted: #{self.is_accepted}"
-
-  end
-
+  include Moderable
 end
